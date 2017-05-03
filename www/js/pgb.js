9cf2d5d823ab7bab514b07a1d4a4733bf3081539
WSEI10141 <puzle99@go2.pl>
2017-05-03 14:10:28 +0200

    8chords

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -3,37 +3,18 @@ function init() {
 }
 
 function onDeviceReady() {
-	navigator.notification.beep(1);
+	navigator.notification.beep(2);
+	deviceInfo();
 }
 
-function netInfo() {
+function deviceInfo() {
 
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
+	info =  'Device Model   : '    + device.model + '<br>' + 
+			'Device Name    : '     + device.name + '<br>' + 
+			'Device Cordova : '  + device.cordova + '<br>' + 
+			'Device Platform: ' + device.platform + '<br>' + 
+			'Device UUID    : '     + device.uuid + '<br>' + 
+			'Device Version : '  + device.version + '<br>';
 
-	navigator.notification.alert(info);
-	
+	document.getElementById("deviceDetails").innerHTML = info;	
 }
-function checkConnection() {
-    var networkState = navigator.connection.type;
- 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
- 
-    alert('Connection type: ' + states[networkState]);
-}
- 
-checkConnection();
