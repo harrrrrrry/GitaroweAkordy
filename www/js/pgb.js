53f174b52fb32d8190fd4ed257daca4189b095e7
WSEI10141 <puzle99@go2.pl>
2017-05-29 18:05:22 +0200

    53

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -45,21 +45,6 @@ function init() {
 	document.addEventListener("deviceready",onDeviceReady, false);
 }
 
-
-function netInfo() {
-
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
-
-	navigator.notification.alert(info);
-	
-}
 function checkConnection() {
     var networkState = navigator.connection.type;
  
